4348fe1a47fc4e614e34c59113dcb5c8cbc59460
Ethan Henderson <ethan@zbee.codes>
2025-08-06 15:41:13 -0600

    fix(Sig): Updated Ground Target Queuing Patch for 7.3

--- MOAction/MOActionAddressResolver.cs
+++ MOAction/MOActionAddressResolver.cs
@@ -9,13 +9,13 @@ public class MOActionAddressResolver
 
     public MOActionAddressResolver(ISigScanner sig)
     {
-        //7.2
-        //this specific adress is inside the useAction function, in there exists a switch statement, 
-        //on abilities (4u) there's a small note to set a boolean depending on if it's a ground target spell or not
+        //7.3
+        //This specific address is inside the useAction function, in there exists a switch statement:
+        //on abilities (4u) there's a small operation to set a boolean, depending on if it's a ground target spell or not.
         //case 4u:
         //   v24 = v23 ^ 1;
         //   break;
         //this signature refers to the XOR operator in said check
-        GtQueuePatch = sig.ScanModule("0F B6 C2 34 01 84 C0 74 8C");
+        GtQueuePatch = sig.ScanModule("0F B6 C2 34 01 84 C0 0F 84");
     }
 }
