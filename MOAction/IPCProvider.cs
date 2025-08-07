3b07d7be890a10d541cb138844f211c1b3432764
Alluneve <buelens7@gmail.com>
2025-08-07 16:10:13 +0200

    IPC provider adding Duty Actions to getRetargetedActions

--- MOAction/IPCProvider.cs
+++ MOAction/IPCProvider.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Ipc;
+using FFXIVClientStructs.FFXIV.Client.Game;
 
 namespace MOAction;
 
@@ -26,20 +27,41 @@ public static class IPCProvider
         Plugin.MoAction.Stacks.ForEach(stack =>
         {
             // Add the base action
-            var baseAction = stack.BaseAction;
-            if (retargetedActions.Contains(baseAction.RowId) || baseAction.ActionType != FFXIVClientStructs.FFXIV.Client.Game.ActionType.Action)
+            var baseRecord = stack.BaseAction;
+            if (retargetedActions.Contains(baseRecord.RowId))
                 return;
 
-            retargetedActions.Add(baseAction.RowId);
+            if (baseRecord.ActionType == ActionType.GeneralAction)
+            {
+                if (Utils.GetDutyActionRow(baseRecord.RowId, out var action))
+                {
+                    retargetedActions.Add(action.RowId);
+                }
+            }
+            else
+            {
+                retargetedActions.Add(baseRecord.RowId);
+            }
+
 
             stack.Entries.ForEach(entry =>
             {
                 // Add the action from the stack entry
-                var actionId = entry.Action.RowId;
-                if (actionId == baseAction.RowId || retargetedActions.Contains(actionId))
+                var record = entry.Action;
+                if (record.RowId == baseRecord.RowId || retargetedActions.Contains(record.RowId))
                     return;
 
-                retargetedActions.Add(actionId);
+                if (record.ActionType == ActionType.GeneralAction)
+                {
+                    if (Utils.GetDutyActionRow(baseRecord.RowId, out var action))
+                    {
+                        retargetedActions.Add(action.RowId);
+                    }
+                }
+                else
+                {
+                    retargetedActions.Add(record.RowId);
+                }
             });
         });
 
